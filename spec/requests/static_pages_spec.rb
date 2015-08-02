aacee91e2d2d4ac6f9c7447e2d2d21e9106fa311
Maksym Mazurkevych <mmmazurkevych@gmail.com>
2015-08-02 20:18:51 +0300

    change homePage title and simpify staticPages tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,18 +4,26 @@ describe "StaticPages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home page" do
 
     before { visit root_path }
+    let(:heading)    { 'Stackoverflow' }
+    let(:page_title) { '' }
+    it { should_not have_title('| Home') }
 
-    it { should have_content('Stackoverflow') }
-    it { should have_title(full_title('Home')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "Help page" do
     before { visit help_path }
+    let(:heading)    { 'Help' }
+    let(:page_title) { 'Help' }
 
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
+    it_should_behave_like "all static pages"
   end
 end

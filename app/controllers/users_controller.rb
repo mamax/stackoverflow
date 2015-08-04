97b20ef34b0843659f2325dc3695ef9dbcfe5d38
Maksym Mazurkevych <mmmazurkevych@gmail.com>
2015-08-04 08:54:08 +0300

    friendly pereaddressation 9.2.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -13,7 +14,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Staoverflow!"
+      flash[:success] = "Welcome to the Stackoverflow!"
       redirect_to @user
     else
       render 'new'
@@ -43,9 +44,14 @@ class UsersController < ApplicationController
 
   def signed_in_user
     unless signed_in?
-      flash[:notice] = "Please sign in."
-      redirect_to signin_url
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
     end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 
 
